b3ea6c07e1b0bce0afbc224cf804435709fcf998
Millie Chan <mireechan@gmail.com>
2016-09-29 11:52:04 -0700

    Add public/bundle.js to index.html

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,20 +1,11 @@
 /* global React ReactDOM */
 
 var div = React.DOM.div
-var h1 = React.DOM.h1
+var MyTitle = require('./MyTitle')
 
 // all components in react must return a component and have a render function
 // MyTitle is a composite component
 
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null,
-        h1({style: {color: this.props.color}}, this.props.title)
-      )
-    )
-  }
-})
 
 var MyTitleFact = React.createFactory(MyTitle)
 // factory when called gives you back the component
